7d38aaab0330abb007c63e04393541a0efd6564e
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-02-29 10:57:01 -0300

    pacotes finalizado

--- fundamentos/1 - pacotes/main.go
+++ fundamentos/1 - pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("lucas@gmail.com")
+	fmt.Println(erro)
 }
